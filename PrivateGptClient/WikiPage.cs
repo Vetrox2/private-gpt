184313aab64bb46e23c550d7afe6266201c3a483
Vetrox2 <mateuszmulka@spoko.pl>
2025-05-07 17:21:12 +0200

    feat(models): add models for azure wiki page

---
+++ PrivateGptClient/WikiPage.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+public class WikiPage
+{
+    [JsonProperty("content")]
+    public string Content { get; set; }
+
+    [JsonProperty("gitItemPath")]
+    public string GitItemPath { get; set; }
+
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("isNonConformant")]
+    public bool IsNonConformant { get; set; }
+
+    [JsonProperty("isParentPage")]
+    public bool IsParentPage { get; set; }
+
+    [JsonProperty("order")]
+    public int Order { get; set; }
+
+    [JsonProperty("path")]
+    public string Path { get; set; }
+
+    [JsonProperty("remoteUrl")]
+    public string RemoteUrl { get; set; }
+
+    [JsonProperty("subPages")]
+    public List<WikiPage> SubPages { get; set; }
+
+    [JsonProperty("url")]
+    public string Url { get; set; }
+}
---
+++ PrivateGptClient/WikiPageNode.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+public class WikiPageNode
+{
+    [JsonProperty("path")]
+    public string Path { get; set; }
+
+    [JsonProperty("order")]
+    public int Order { get; set; }
+
+    [JsonProperty("gitItemPath")]
+    public string GitItemPath { get; set; }
+
+    [JsonProperty("isParentPage")]
+    public bool? IsParentPage { get; set; }
+
+    [JsonProperty("subPages")]
+    public List<WikiPageNode> SubPages { get; set; }
+
+    [JsonProperty("url")]
+    public string Url { get; set; }
+
+    [JsonProperty("remoteUrl")]
+    public string RemoteUrl { get; set; }
+}
